42a32b4206ccd544e3ba9e2b83fcd63af31b6379
evio <shenyunjie@51zhangdan.com.cn>
2016-03-07 13:06:42 +0800

    update

--- src/js/components/checkbox.js
+++ src/js/components/checkbox.js
@@ -1,16 +1,14 @@
 var utils = require('../utils');
 exports.name = "ui-checkbox";
-exports.props = ['raw', 'value', 'shape', 'type', 'disvalue'];
+exports.props = ['raw', 'value', 'disvalue'];
 exports.template =
-    '<div class="ui-checkbox" :class="classes" v-el:root>' +
+    '<div class="ui-checkbox" :class="{active:compute}" v-el:root>' +
         '<label></label>' +
     '</div>';
 
 exports.ready = function(){
     var that = this;
-    if ( !this.checked ) this.value = '1';
-    if ( !this.shape ) this.shape = 'round'; // round square
-    if ( !this.type ) this.type = 'hook'; // hook round
+    if ( !this.value ) this.value = '1';
     if ( !this.disvalue ) this.disvalue = '';
     this._cb = function(e){
         if ( that.compute ){
@@ -29,12 +27,5 @@ exports.beforeDestroy = function(){
 exports.computed = {
     compute: function(){
         return this.raw == this.value;
-    },
-    classes: function(){
-        var classes = [this.shape, this.type];
-        if ( this.compute ){
-            classes.push('active');
-        }
-        return classes.join(' ');
     }
 }
