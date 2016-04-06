16a54c7e5fc35283914715e8e8500997b92ae166
伍峰杰 <wufengjie@u51.com>
2016-04-06 13:35:32 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,23 @@
 import * as simplize from './main.js';
 import fetcher from './require';
 
-simplize.browser('sync', function(resolve){
+simplize.browser('sync', function(resolve) {
     fetcher(['http://192.168.20.57:8000/js/sync.js'], resolve);
 });
 
-simplize.webview('home', 'list', function(resolve){
+simplize.webview('home', 'list', function(resolve) {
     fetcher(['http://192.168.20.57:8000/js/list.js'], resolve);
 });
 
 let picker_data = [];
-for(let pi = 0; pi < 10; pi++){
+for (let pi = 0; pi < 10; pi++) {
     picker_data.push({
         value: pi + 1,
         text: "第" + (pi + 1) + '个数据'
     })
 }
 let picker_data2 = [];
-for(let pi = 0; pi < 150; pi++){
+for (let pi = 0; pi < 150; pi++) {
     picker_data2.push({
         value: pi + 1,
         text: "第" + (pi + 1) + '个数据'
@@ -37,19 +37,19 @@ const resource = {
                 zindex: 2,
                 template: require('../html/index.html'),
                 events: {
-                    "webview:preload": function(){
+                    "webview:preload": function() {
                         console.log('index preload')
                     },
-                    "webview:loading": function(){
+                    "webview:loading": function() {
                         console.log('index loading')
                     },
-                    "webview:load": function(){
+                    "webview:load": function() {
                         console.log('index load');
-                        this.$alert('Hello world!').then(function(Alert){
+                        this.$alert('Hello world!').then(function(Alert) {
                             console.log(Alert);
                         })
                     },
-                    "webview:preset": function(head, tool){
+                    "webview:preset": function(head, tool) {
                         head.active();
                         tool.active();
                         head.data.left.icon = '<i class="fa fa-close"></i>';
@@ -65,10 +65,10 @@ const resource = {
                 zindex: 3,
                 template: require('../html/info.html'),
                 events: {
-                    "webview:load": function(){
+                    "webview:load": function() {
                         console.log('info load')
                     },
-                    "webview:preset": function(head){
+                    "webview:preset": function(head) {
                         head.active();
                         head.data.left.icon = '<i class="fa fa-send"></i>';
                         head.data.left.text = 'Back';
@@ -80,7 +80,7 @@ const resource = {
                         head.data.center.text = 'Simplize info';
                         this.SP_webviewContentClass = 'test2';
                     },
-                    "webview:refresh": function(){
+                    "webview:refresh": function() {
                         console.log('refresh');
                     }
                 }
@@ -92,26 +92,26 @@ const resource = {
                     index: 3
                 },
                 events: {
-                    "webview:preset": function(headbar, toolbar){
+                    "webview:preset": function(headbar, toolbar) {
                         headbar.active();
                         toolbar.active();
                         headbar.data.left.icon = '<i class="fa fa-angle-left"></i>';
                         headbar.data.left.text = 'Back';
-                        headbar.data.left.click = function(){
+                        headbar.data.left.click = function() {
                             history.back();
                         }
                         headbar.data.center.text = 'Picker scroller';
                         this.SP_webviewContentClass = 'test2';
                     },
-                    'webview:load': function(){
-                        this.$refs.picker.$on('scroll:selected', function(data, index){
+                    'webview:load': function() {
+                        this.$refs.picker.$on('scroll:selected', function(data, index) {
                             console.log(data, index)
                         });
                         setTimeout(() => {
                             this.list = picker_data2;
                         }, 10000);
                     },
-                    "webview:unload": function(){
+                    "webview:unload": function() {
                         this.$refs.picker.$off('scroll:selected');
                     }
                 }
@@ -125,20 +125,20 @@ const data = {
 }
 
 
-simplize.ready(function(){
+simplize.ready(function() {
     let app = simplize.bootstrap(resource, data);
 
     app
         .$use('/sync',
             app.$browser('sync')
-                .$define('index')
+            .$define('index')
         )
         .$use(
             app.$browser('home')
-                .$define('/info', 'info')
-                .$define('/list', 'list')
-                .$define('/picker', 'picker')
-                .$define('index')
+            .$define('/info', 'info')
+            .$define('/list', 'list')
+            .$define('/picker', 'picker')
+            .$define('index')
         );
 
     app.$run();
--- src/js/plugins/alert/index.js
+++ src/js/plugins/alert/index.js
@@ -1,15 +1,18 @@
-import { on, off } from '../../utils/index';
+import {
+    on,
+    off
+} from '../../utils/index';
 export let ALERT = {
     name: 'sp-ui-alert',
     template: require('./alert.html'),
-    data(){
+    data() {
         return {
             text: '',
             title: ''
         }
     },
     methods: {
-        entry(text = '', title = ''){
+        entry(text = '', title = '') {
             this.$parent.mask = true;
             this.text = text;
             this.title = title;
