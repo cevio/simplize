d81cdfc99b739a0ce8b67ba52900fbb9c0eb2501
伍峰杰 <wufengjie@u51.com>
2016-04-06 14:48:56 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -45,9 +45,9 @@ const resource = {
                     },
                     "webview:load": function() {
                         console.log('index load');
-                        this.$alert('Hello world!').then(function(Alert) {
-                            console.log(Alert);
-                        })
+                        // this.$alert('Hello world!').then(function(Alert) {
+                        //     console.log(Alert);
+                        // })
 
 
                     },
