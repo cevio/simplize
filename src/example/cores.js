43c754157aee63ccc8ca2177dd443032e12008c1
伍峰杰 <wufengjie@u51.com>
2016-04-11 20:41:48 +0800

    update

--- src/example/cores.js
+++ src/example/cores.js
@@ -30,8 +30,6 @@ export { panel } from "./cores/panel";
 export { progress } from './cores/components/progress';
 export { time } from './cores/components/time';
 export { circle } from './cores/components/circle';
-<<<<<<< HEAD
+
 export { spinner } from './cores/components/spinner';
-=======
 export { rater } from "./cores/components/rater";
->>>>>>> d563782e1a3d32b542084837b35b98737e79a041
