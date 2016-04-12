4c6b83fe4ad8da50610e47e3abb550ac126dc03a
evio <shenyunjie@51zhangdan.com.cn>
2016-04-12 13:36:29 +0800

    update

--- src/js/components/pull/index.js
+++ src/js/components/pull/index.js
@@ -4,18 +4,35 @@ export let pull = {
     template: require('./pull.html'),
     methods: {
         create(){
-            this.$Scroller = new PULL(this, this.$els.page);
+            this.$scroller = new PULL(this, this.$els.root);
+            this.y = 0;
+            this._y = 0;
+            this.refresher = {
+                height: this.$els.refresh.offsetHeight
+            }
         },
         destroy(){
-
-        }
+            this.$scroller.__destroy();
+            this.$scroller = null;
+            this.y = 0;
+            this._y = 0;
+            this.refresher = null;
+        },
+        move(top){
+            this.$els.page.style.webkitTransform = 'translate3d(0, ' + top + 'px, 0)';
+        },
     },
     events: {
         "webview:load": function(){
             this.create();
         },
         "scroll:move": function(e){
-            console.log(e);
+            this.y = e.currentY - e.startY + this._y;
+            this.move(this.y);
+            console.log(this.y, this.refresher.height)
+        },
+        "scroll:end": function(){
+            this._y = this.y;
         }
     }
 }
