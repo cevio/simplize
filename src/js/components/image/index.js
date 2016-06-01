4b2511281d770ff99bc74e5729a556619adccc07
guanyx <476848761@163.com>
2016-06-01 21:22:07 +0800

    update

--- src/js/components/image/index.js
+++ src/js/components/image/index.js
@@ -13,16 +13,37 @@ export let photo = {
             type: String,
             default: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
         }
+    },
+    ready: function(){
+        this.$dispatch('photo:ready');
     }
 }
 export let photos = {
     name: 'photos',
     template: '<div class="sp-images" v-el:root><slot></slot></div>',
     events: {
-        "webview:load": function(){
-            this.$lazyimage = new blazy({
-                container: this.$els.root
-            });
+        "photo:ready": function(){
+            this.reinit();
+        }
+    },
+    ready: function(){
+        this.$lazyimage = new blazy({
+            container: this.$els.root
+        });
+    },
+    data: function(){
+        return {
+            timer: null
+        }
+    },
+    methods: {
+        reinit: function(){
+            clearTimeout(this.timer);
+            
+            this.timer = setTimeout(() => {
+                this.$lazyimage.revalidate();
+                this.$lazyimage._util.validateT();
+            }, 25);
         }
     }
 }
